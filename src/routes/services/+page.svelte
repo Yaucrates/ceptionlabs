a6070d7ebe77e3bad1afb93e5d94e034579ae53c
Yaucrates <abdulhusseinyousifh@gmail.com>
2024-11-12 21:01:30 -0500

    moonlight scene

--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -1,34 +1,40 @@
 <script>
     import MoonlightScene from "$lib/Components/MoonlightScene/MoonlightScene.svelte";
     import TopRightArrow from "$lib/Logos/TopRightArrow.svelte";
-
 </script>
-<main class="h-full flex flex-col md:flex-row">
-    <div class="w-full md:w-1/2 h-[36rem] md:h-full flex justify-center items-center overflow-clip relative">
+
+<main class="h-full flex flex-col lg:flex-row">
+    <div class="w-full lg:w-1/2 h-[36rem] lg:h-full flex justify-center items-center overflow-clip relative">
         <MoonlightScene />
-        <h1 class="text-7xl text-white font-black text-end uppercase bottom-6 right-8 md:bottom-12 md:right-16 absolute z-20">Explore<br/>Our Services</h1>
+        <h1 class="text-5xl min-[500px]:text-6xl sm:text-7xl text-white font-black text-end uppercase bottom-6 right-8 lg:bottom-12 lg:right-16 absolute z-20">Explore<br/>Our Services</h1>
     </div>
-    <div class="w-full md:w-1/2 flex flex-col border-l-0 md:border-l-[1px] border-ception-glassyWhite box-border">
-        <div class="p-8 h-[15.5rem] md:h-1/3 flex flex-col justify-between md:border-t-0 border-t-[1px] border-ception-glassyWhite box-border text-white uppercase">
-            <h3 class="w-1/2 text-xl font-medium">Maintenance & Hosting Service</h3>
+    <div class="w-full lg:w-1/2 flex flex-col border-l-0 lg:border-l-[1px] border-ception-glassyWhite box-border">
+        <div class="p-8 min-h-[15.5rem] lg:min-h-0 lg:h-1/3 gap-4 flex flex-col justify-between lg:border-t-0 border-t-[1px] border-ception-glassyWhite box-border text-white uppercase">
+            <h3 class="text-xl font-medium">Maintenance & Hosting<br/>Service</h3>
             <div class="w-full flex justify-end items-center gap-4">
-                <p class="w-1/2 text-end">Reliable 24/7 hosting and maintenance.</p>
-                <TopRightArrow width={60}/>
+                <p class="text-end">Reliable 24/7 hosting and <br class="hidden min-[460px]:block"/>maintenance.</p>
+                <div class="min-w-[60px] min-h-[60px] flex items-center justify-center">
+                    <TopRightArrow width={60} />
+                </div>
             </div>
         </div>
-        <div class="bg-cyan-900 p-8 h-[15.5rem] md:h-1/3 flex flex-col justify-between text-white uppercase">
-            <h3 class="w-3/5 font-bold text-2xl">Web Design & Development Service</h3>
+        <div class="bg-cyan-900 p-8 min-h-[15.5rem] lg:min-h-0 lg:h-1/3 gap-4 flex flex-col justify-between text-white uppercase">
+            <h3 class="font-bold text-2xl">Web Design & Development<br/>Service</h3>
             <div class="w-full flex justify-end items-center gap-4">
-                <p class="w-1/2 text-end">Custom design and development to bring your website vision to life.</p>
-                <TopRightArrow width={60}/>
+                <p class="text-end">Custom design and development to <br class="hidden min-[460px]:block"/>bring your website vision to life.</p>
+                <div class="min-w-[60px] min-h-[60px] flex items-center justify-center">
+                    <TopRightArrow width={60} />
+                </div>
             </div>
         </div>
-        <div class="p-8 h-[15.5rem] md:h-1/3 flex flex-col justify-between text-white uppercase">
-            <h3 class="w-1/2 text-xl font-medium">Additional Services to Meet Your Needs</h3>
+        <div class="p-8 min-h-[15.5rem] lg:min-h-0 lg:h-1/3 gap-4 flex flex-col justify-between text-white uppercase">
+            <h3 class="text-xl font-medium">Additional Services to Meet<br/>Your Needs</h3>
             <div class="w-full flex justify-end items-center gap-4">
-                <p class="w-1/2 text-end">Comprehensive support for any additional website requirements.</p>
-                <TopRightArrow width={60}/>
+                <p class="text-end">Comprehensive support for any <br class="hidden min-[460px]:block"/>additional website requirements.</p>
+                <div class="min-w-[60px] min-h-[60px] flex items-center justify-center">
+                    <TopRightArrow width={60} />
+                </div>
             </div>
-        </div>        
+        </div>
     </div>
 </main>
