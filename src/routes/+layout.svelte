e26f0a57e0f6940f09048cfce38e07539f49f68e
Yaucrates <abdulhusseinyousifh@gmail.com>
2024-12-07 19:50:53 -0500

    loading inter

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,12 @@
     let { children } = $props();
 </script>
 
+<svelte:head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+</svelte:head>
+
 <div class="min-h-screen flex flex-col">
     <Header />
     <div class="mt-[76px] flex-grow relative">
